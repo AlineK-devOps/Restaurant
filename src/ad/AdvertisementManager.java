da7c3b405288db8f24632ad38ec2e55547869d34
Aline Korshunova <aline.korshunova00@gmail.com>
2021-06-11 16:05:23 +0300

    Implemented AdvertisementStorage

--- src/ad/AdvertisementManager.java
+++ src/ad/AdvertisementManager.java
@@ -1,4 +1,5 @@
 package ad;
 
 public class AdvertisementManager { //менеджер для оптимального подбора роликов для каждого заказа
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance(); //хранилище рекламных роликов
 }
--- src/ad/AdvertisementStorage.java
+++ src/ad/AdvertisementStorage.java
@@ -1,9 +1,16 @@
 package ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage { //хранилище рекламных роликов
     private static AdvertisementStorage advertisementStorage;
+    private final List<Advertisement> videos = new ArrayList<>(); //список доступных рекламных роликов
 
     private AdvertisementStorage() {
+        videos.add(new Advertisement(new Object(), "First Video", 5000, 100, 3 * 60));
+        videos.add(new Advertisement(new Object(), "Second Video", 100, 10, 15 * 60));
+        videos.add(new Advertisement(new Object(), "Third Video", 400, 2, 10 * 60));
     }
 
     public static AdvertisementStorage getInstance(){
@@ -11,5 +18,13 @@ public class AdvertisementStorage { //хранилище рекламных ро
             advertisementStorage = new AdvertisementStorage();
 
         return advertisementStorage;
+    }
+
+    public List<Advertisement> list(){ //получение списка доступных роликов
+        return videos;
+    }
+
+    public void add(Advertisement advertisement){ //добавление нового ролика в список
+        videos.add(advertisement);
     }
 }
