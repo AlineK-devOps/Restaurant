4ca064f31bedb208e2de5292a6e6faa6a04b75dc
Aline Korshunova <aline.korshunova00@gmail.com>
2021-06-12 23:05:12 +0300

    Added event storage

--- src/statistic/StatisticManager.java
+++ src/statistic/StatisticManager.java
@@ -1,8 +1,26 @@
 package statistic;
 
 import statistic.event.EventDataRow;
+import statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager { //регистрация событий в хранилище
+
+    private class StatisticStorage{ //хранилище
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType event : EventType.values()){
+                storage.put(event, new ArrayList<>());
+            }
+        }
+    }
+
+    private StatisticManager.StatisticStorage statisticStorage = new StatisticStorage();
     private static StatisticManager manager; //Singleton
 
     private StatisticManager() {
